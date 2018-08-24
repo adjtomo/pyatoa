5b4bee393a522de4f3c5c5ba97cd25e33e97a76c
Bryant Chow <bryant.h.c@gmail.com>
2018-08-24 14:32:42 +1200

    Initializing repository

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyatoa',
+      version='0.1',
+      description="Python's Adjoint Tomography Operational Assitance"
+      url='http://github.com/bch0w/pyatoa',
+      author='Bryant Chow',
+      author_email='bryant.chow@vuw.ac.nz',
+      license='GPL',
+      packages=['pyatoa'],
+      zip_safe=False)
