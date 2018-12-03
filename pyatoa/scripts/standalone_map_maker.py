bb0d11dea3cf0527d83a957f9e2d05266d9e0325
Bryant Chow <bryant.h.c@gmail.com>
2018-12-04 12:22:32 +1300

    standalone map maker merge

--- pyatoa/scripts/standalone_map_maker.py
+++ pyatoa/scripts/standalone_map_maker.py
@@ -3,4 +3,4 @@ config = pyatoa.Config()
 mgmt = pyatoa.Manager(config=config, empty=True)
 mgmt.plot_map(show=True, show_faults=False, color_by_network=True,
               figsize=(10, 9.4), dpi=100,
-              map_corners=[-41.75,-38.25,174.5,178.1])
+              map_corners=[-41.75,-38.9,174.5,177.5])
