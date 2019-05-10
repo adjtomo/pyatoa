f41fd9253f4a404e49f37ed21751f9769324b8c4
bch0w <bryant.h.c@gmail.com>
2019-05-10 01:19:56 +0000

    seisflows plugin working

--- pyatoa/scripts/run_process_seisflows.sh
+++ pyatoa/scripts/run_process_seisflows.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=pyatoa_test
-#SBATCH --time=00:05:00
+#SBATCH --time=00:10:00
 #SBATCH --tasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --account=nesi00263    
@@ -9,11 +9,11 @@
 #SBATCH --export=None
 
 # Kinda hacky but parameters are taken from positional arguments
-event_id = "$1"
-model_number = "$2"
-syn_dir = "$3"
-work_dir = "$4"
-out_dir = "$5"
+event_id="$1"
+model_num="$2"
+syn_dir="$3"
+work_dir="$4"
+out_dir="$5"
 
 # load anaconda and hdf5, start conda environment
 echo "loading modules"
@@ -21,8 +21,8 @@ module load Anaconda3/5.2.0-GCC-7.1.0
 module load HDF5/1.10.1-GCC-7.1.0 
 source activate tomo
 
-python /scale_wlg_nobackup/filesets/nobackup/nesi00263/bchow/tests/pyatoa_test/process_maui.py \
--l ${event_id} -m ${model_num} -p ${syn_dir} -w ${work_dir} -o ${out_dir}
+python /scale_wlg_nobackup/filesets/nobackup/nesi00263/bchow/tests/pyatoa_test/process_seisflows.py \
+-i ${event_id} -m ${model_num} -p ${syn_dir} -w ${work_dir} -o ${out_dir}
 
 
 echo "finished `date`"
