572544df81ebc176c18ce2f1a96cf8f33d87c6cd
Bryant Chow <bryant.h.c@gmail.com>
2020-02-04 12:51:58 +1300

    removed unit test testing, starting testing from scratch using pytest

--- pyatoa/tests/test_config.py
+++ pyatoa/tests/test_config.py
@@ -1,85 +1,48 @@
 """
-Test the Config class, initialization, checks, reading and writing
+Test the Config object from Pyatoa
 """
-import os
-import pyasdf
-import unittest
-from pyatoa import Config
+import pytest
+from pyatoa.core.config import Config
 
 
-class TestConfig(unittest.TestCase):
-    def setUp(self):
-        """
-        Set up the class
-        """
-        self.test_data_path = os.path.join(os.path.abspath(
-            os.path.dirname(__file__)), 'data', '')
-
-        self.empty_ds_fid = os.path.join(
-            self.test_data_path, 'test_empty_ds.h5'
-        )
-
-    def test_print(self):
-        """
-        Testing the string representation
-        """
-        cfg = Config()
-        print(cfg)
-
-    def test_init(self):
-        """
-        Make sure config class can be initiated with various model numbers
-        """
-        model_number_check = "m00"
-        for model_number in [0, '0', 'm00']:
-            config = Config(model_number=model_number)
-            self.assertTrue(config.model_number, model_number_check)
-
-    def test_check(self):
-        """
-        Test the check function by providing incorrectly formated parameters
-        """
-        with self.assertRaises(AssertionError):
-            # Check period range
-            Config(min_period=100, max_period=10)
-            # Check map corner dictionary
-            Config(map_corners={'test': None})
-            # Check unit output
-            Config(unit_output='test')
-            # Check synthetic output
-            Config(cfgpaths={'test': 'test'})
-            # Check window amplitude ratio
-            Config(window_amplitude_ratio=-1)
-            Config(window_amplitude_ratio=2)
-            # External configs
-            Config(pyflex_map='test')
-            Config(adj_src_type='test')
-
-    def test_read_write(self):
-        """
-        Ensure that the config can be written in each format
-        """
-        cfg = Config()
-        for fid in ["test_config.yaml", "test_config.txt"]:
-            cfg.write(fid)
-            cfg.read(fid)
-            if "yaml" in fid:
-                # Make sure you can initiate using a yaml file
-                Config(yaml_fid="test_config.yaml")
-            os.remove(fid)
-
-        # reading and writing from ASDF
-        ds = pyasdf.ASDFDataSet(self.empty_ds_fid)
-        cfg = Config()
-        cfg.write(ds)
-        cfg.read(ds, path="default")
-        os.remove(self.empty_ds_fid)
+def set_and_check(config, attribute, value):
+    """
+    Convenience function to set an attribute and run the config check
+    """
+    setattr(config, attribute, value)
+    config._check()
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_print():
+    """
+    Test the print statement of the config object
+    """
+    assert Config().__str__()
 
 
+def test_read_yaml():
+    """
+    Test reading the Config object from a yaml file
+    """
+    for yaml_fid in ["./test_data/sfconfig.yaml",
+                     "./test_data/seisflows/parameters.yaml"]:
+        cfg = Config(yaml_fid)
+        assert cfg
 
+
+def test_check():
+    """
+    Check that incorrect values will create errors in the Config check
+    """
+    cfg = Config()
+
+    with pytest.raises(AssertionError):
+        set_and_check(cfg, "min_period", 100)
+        set_and_check(cfg, "max_period", 10)
+        set_and_check(cfg, "map_corners", {"test": 5})
+        set_and_check(cfg, "unit_output", "test")
+        set_and_check(cfg, "synthetic_unit", "test")
+        set_and_check(cfg, "cfgpaths", [])
+        set_and_check(cfg, "window_amplitude_ratio", 1E3)
 
 
--- pyatoa/tests/test_manager.py
+++ pyatoa/tests/test_manager.py
@@ -1,235 +1,64 @@
 """
-Make sure the Manager class works as advertised
+Test the functionalities of the Pyaflowa Manager class
 """
-import unittest
-
-import os
-import obspy
+import pytest
 import pyasdf
 from pyatoa import Config, Manager
-from pyatoa.utils.asdf.deletions import clean_ds
+from obspy import read, read_events, read_inventory
 
 
-class TestManager(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        """
-        set up the class
-        :return:
-        """
-        cls.test_data_path = os.path.join(os.path.abspath(
-            os.path.dirname(__file__)), 'data', '')
-        cls.inv = obspy.read_inventory(
-            os.path.join(cls.test_data_path, 'test_inv.xml')
-        )
-        cls.catalog = obspy.read_events(
-            os.path.join(cls.test_data_path, 'test_cat.xml')
-        )
-        cls.event = cls.catalog[0]
-        cls.st_obs = obspy.read(
-            os.path.join(cls.test_data_path, 'test_obs_data.ascii')
-        )
-        cls.st_syn = obspy.read(
-            os.path.join(cls.test_data_path, 'test_syn_m00_data.ascii')
-        )
-        cls.config = Config(
-            model_number="m00",
-            event_id=cls.event.resource_id.resource_id.split('/')[1],
-
-        )
-        cls.empty_ds_fid = os.path.join(
-            cls.test_data_path, 'test_empty_ds.h5'
-        )
-
-    @classmethod
-    def tearDownClass(cls):
-        """
-        Testing finalizations
-        :return:
-        """
-        # Remove the empty dataset
-        if os.path.exists(cls.empty_ds_fid):
-            os.remove(cls.empty_ds_fid)
-
-    def test_empty_manager(self):
-        """
-        Check empty Manager and calling commands with no data
-        """
-        # Empty manager initiation
-        with self.assertRaises(TypeError):
-            Manager()
-
-        # Test that `empty` gathering works
-        mgmt = Manager(config=self.config, empty=True)
-        self.assertIsNone(mgmt.event)
-
-        # Assert empty Manager returns nothing
-        self.assertIsNone(mgmt.standardize())
-        self.assertIsNone(mgmt.preprocess())
-        self.assertIsNone(mgmt.window())
-        self.assertIsNone(mgmt.measure())
-        self.assertIsNone(mgmt.plot())
-        self.assertIsNone(mgmt.map())
-
-    def test_standardize(self):
-        """
-        Make sure the standardize function returns two traces that have the
-        same sampling rate and number of points
-        """
-        mgmt = Manager(config=self.config, empty=True, st_obs=self.st_obs,
-                       st_syn=self.st_syn)
-
-        # Make sure that the raw traces are not standardized
-        for comp in mgmt.config.component_list:
-            obs = mgmt.st_obs.select(component=comp)[0]
-            syn = mgmt.st_obs.select(component=comp)[0]
-            self.assertFalse(obs.stats.sampling_rate != syn.stats.sampling_rate)
-            self.assertFalse(obs.stats.npts != syn.stats.npts)
-
-        mgmt.standardize()
-
-        # Make sure the new traces are standardized
-        for comp in mgmt.config.component_list:
-            obs = mgmt.st_obs.select(component=comp)[0]
-            syn = mgmt.st_obs.select(component=comp)[0]
-            self.assertTrue(obs.stats.sampling_rate == syn.stats.sampling_rate)
-            self.assertTrue(obs.stats.npts == syn.stats.npts)
-
-    def test_preprocess(self):
-        """
-        Make sure that preprocess function works
-        """
-        mgmt = Manager(config=self.config, empty=True, event=self.event,
-                       st_obs=self.st_obs, st_syn=self.st_syn, inv=self.inv)
-        mgmt.standardize()
-        mgmt.preprocess()
-
-        # Check that the proper processing has occurred
-        for tr in mgmt.st_obs:
-            self.assertTrue(hasattr(tr.stats, 'processing'))
-            self.assertEqual(len(tr.stats.processing), 13)
-
-        for tr in mgmt.st_syn:
-            self.assertTrue(hasattr(tr.stats, 'processing'))
-            self.assertEqual(len(tr.stats.processing), 9)
-
-    def test_preprocess_unit(self):
-        """
-        Ensure that preprocess returns the correct component
-        """
-        config = self.config
-        config.output_unit = "DISP"
-        config.synthetic_unit = "VEL"
-
-        comp = "Z"
-        mgmt = Manager(config=self.config, empty=True, event=self.event,
-                       st_obs=self.st_obs, st_syn=self.st_syn, inv=self.inv)
-        mgmt.standardize()
-        mgmt.preprocess()
-
-        # Make sure preprocess recognized different units and adjusted syn's
-        self.assertNotEqual(mgmt.st_syn.select(component=comp)[0].max(),
-                            self.st_syn.select(component=comp)[0].max())
-
-    def test_zero_pad(self):
-        """
-        Ensure that zero padding works in preprocess
-        """
-        config = self.config
-
-        config.zero_pad = 50
-        expected_length = 42500
-        mgmt = Manager(config=self.config, empty=True, event=self.event,
-                       st_obs=self.st_obs, st_syn=self.st_syn, inv=self.inv)
-        mgmt.standardize()
-        mgmt.preprocess()
-
-        # Make sure preprocess recognized different units and adjusted syn's
-        self.assertEqual(len(mgmt.st_syn[0].data), expected_length)
-
-    def test_window(self):
-        """
-        Make sure Pyflex returns expected windows from a "default" config
-        """
-        assert(self.config.pyflex_config[0] == "default")
-        assert(self.config.pyadjoint_config[0] == "multitaper_misfit")
-
-        mgmt = Manager(config=self.config, empty=True, event=self.event,
-                       st_obs=self.st_obs, st_syn=self.st_syn, inv=self.inv)
-        mgmt.standardize()
-        mgmt.preprocess()
-
-        # Simple check to make sure Pyflex found windows using default params
-        expected_windows = 3
-        mgmt.window()
-        self.assertEqual(len(mgmt.windows), expected_windows)
-        self.assertEqual(len(mgmt.staltas), expected_windows)
-        self.assertEqual(mgmt.num_windows, expected_windows)
-
-    def test_measure(self):
-        """
-        Ensure Pyadjoint returns the correct misfit for given windows
-        """
-        misfit_check = 4.115253378447238
-
-        assert(self.config.pyflex_config[0] == "default")
-        assert(self.config.pyadjoint_config[0] == "multitaper_misfit")
-
-        mgmt = Manager(config=self.config, empty=True, event=self.event,
-                       st_obs=self.st_obs, st_syn=self.st_syn, inv=self.inv)
-        mgmt.standardize()
-        mgmt.preprocess()
-
-        mgmt.measure()
-        self.assertEqual(len(mgmt.adj_srcs), 3)
-        self.assertAlmostEqual(mgmt.misfit, misfit_check)
-
-    def test_fill_dataset(self):
-        """
-        Test pyatoa's ability to fill a PyASDF ASDFDataSet
-        Will query FDSN
-        :return:
-        """
-        with pyasdf.ASDFDataSet(self.empty_ds_fid) as test_ds:
-            clean_ds(test_ds)
-            mgmt = Manager(config=self.config, ds=test_ds, empty=True,
-                           event=self.event, st_obs=self.st_obs,
-                           st_syn=self.st_syn, inv=self.inv
-                           )
-            mgmt.populate()
-            mgmt.standardize()
-            mgmt.preprocess()
-            mgmt.window()
-
-            # Check that the dataset contains the correct auxiliary data
-            check_model = self.config.model_number
-            check_window = 'NZ_BFZ_E_0'
-            check_param = ("left_index", 1596)
-
-            self.assertTrue(hasattr(mgmt.ds.auxiliary_data, 'MisfitWindows'))
-            self.assertTrue(hasattr(mgmt.ds.auxiliary_data.MisfitWindows,
-                                    check_model))
-            self.assertTrue(hasattr(
-                mgmt.ds.auxiliary_data.MisfitWindows[check_model], check_window)
-            )
-
-            # Shorten call for cleanliness, check that misfit windows picked ok
-            wd = mgmt.ds.auxiliary_data.MisfitWindows[check_model][check_window]
-            self.assertTrue(hasattr(wd, 'parameters'))
-            self.assertEqual(wd.parameters[check_param[0]], check_param[1])
-
-            # Remove the contents of the test database for future testing
-            clean_ds(test_ds)
-        del test_ds
+@pytest.fixture
+def obs():
+    """Real data stream"""
+    return read("./test_data/test_obs_data _NZ_BFZ_2018p130600.ascii")
 
 
-if __name__ == "__main__":
-    unittest.main()
+@pytest.fixture
+def syn():
+    """Synthetic data stream"""
+    return read("./test_data/test_syn_data _NZ_BFZ_2018p130600.ascii")
 
 
+@pytest.fixture
+def cat():
+    """Event Catalog"""
+    return read_events("./test_data/test_event_2018p130600.xml")
 
 
+@pytest.fixture
+def event(cat):
+    """Event from Catalog"""
+    return cat[0]
 
 
+@pytest.fixture
+def inv():
+    """StationXML information"""
+    return read_inventory("./test_data/test_inv.xml")
 
 
+@pytest.fixture
+def config():
+    """Pyatoa Config object"""
+    return Config(model_number="m00", event_id="2018p130600")
+
+
+@pytest.fixture
+def ds():
+    """Pyasdf ASDFDataSet"""
+    return pyasdf.ASDFDataSet("./test_data/test_empty_dataset.h5")
+
+
+def test_empty():
+    """Ensure that opening an empty Manager doesnt work"""
+    with pytest.raises(TypeError):
+        Manager()
+
+
+def test_init_empty(config):
+    """Ensure that opening a Manager doesnt allow workflow"""
+    mgmt = Manager(config=config, empty=True)
+    assert(mgmt)
+
+
+# def test_
