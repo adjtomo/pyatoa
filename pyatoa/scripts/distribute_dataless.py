bec275542f305abac333cfbd7936f7342c495cf3
Bryant Chow <bryant.h.c@gmail.com>
2020-07-17 12:26:46 +1200

    moved some script functions around

---
+++ pyatoa/scripts/distribute_dataless.py
@@ -0,0 +1,50 @@
+"""
+Write an ObsPy Inventory object, which can be comprised of various 
+
+Response files written through Obspy come out as a single object, but Pyatoa
+will look for response information from individual components and individual
+stations. Distrubute this dataless information into the necessary components
+"""
+import os
+from obspy import read_inventory
+
+def distribute_dataless(inv, path="./"):
+    """
+    Note: The template here is hardcoded with SEED convention
+
+    :type inv: obspy.core.inventory.Inventory
+    :param inv: inventory with response to be delinieated into separate objects
+    :type path: str
+    :param path: path to save the new response files to
+    """
+    # This follows SEED convention
+    inner_folder = "{STA}.{NET}"
+    fid_template = "RESP.{NET}.{STA}.{LOC}.{CHA}"
+
+    full_template = os.path.join(path, inner_folder, fid_template)
+    for net in inv:
+        for sta in net:
+            try:
+                # Create the container directory unless it exists
+                os.mkdir(os.path.join(path, inner_folder.format(STA=sta.code, 
+                                                                NET=net.code))
+                )
+            except FileExistsError:
+                pass
+
+            for cha in sta:
+                # Write the individual channel as a STATIONXML file
+                inv_temp = inv.select(network=net.code, station=sta.code,
+                                      location=cha.location_code,
+                                      channel=cha.code)
+                inv_temp.write(full_template.format(
+                    STA=sta.code, NET=net.code, LOC=cha.location_code,
+                    CHA=cha.code),
+                    format="STATIONXML"
+                    )
+
+if __name__ == "__main__":
+    path = "./"
+    inv = read_inventory("XML FILE NAME HERE")
+
+    distribute_dataless(inv, path)     
---
+++ pyatoa/scripts/make_figures/call_model_maker.py
@@ -0,0 +1,200 @@
+"""
+A script to call the pyatoa.visuals.model.Model class and plot standard looking
+slices of a model for easy visualization. Contains a set of configurations
+to control the look of the figures.
+
+Note:
+    One can easily make a .gif to visualize model slices in psuedo-3d by using
+    the imagemagick package, with the command. This could be wrapped in
+    subprocess but since it's a one liner, it's pretty easy to do quickly.
+
+    $ convert -delay 20 -loop 1 *png model_animated.gif
+"""
+import os
+from glob import glob
+from pyatoa.visuals.model_maker import ModelMaker
+from pyatoa.utils.images import imgs_to_pdf
+
+
+def save_pdf(pngs, tag):
+    """
+    Convenience function to collect pngs into a pdf and then remove pngs
+    :param pngs:
+    :param tag:
+    :return:
+    """
+    imgs_to_pdf(pngs, tag.format(tag="all").replace("png", "pdf"))
+    for png in pngs:
+        os.remove(png)
+
+
+def call_models(fid, path_out="./figures", make_pdf=True):
+    """
+    Set the parameters in this function, which will then be passed to Model
+
+    Slice attributes:
+    :z_slices (list): list of slices to make for top-down figures (normal Z).
+        use "surface" for surface projection, otherwise use floats corresponding
+        to depth. e.g. slices at surface, 5 and 10km depth would be
+        ['surface', 5, 10]
+    :x_slices (list): list of slices for side on figures (normal Y-axis)
+        these are given in percentage of the axis from 0 to 1. e.g. if you want
+        a slice at the middle of the axes, [0.5]
+    :y_slices (list): list of slices for side on figures (normal X-axis)
+
+    Parameter attributes:
+    :font_factor (float): size of font on the axes objects, default 1.1
+    :convert (float): convert the axis by some constant value, e.g. if axes are
+                      in units of meters, use 1E-3 to get into units of km
+    :zero_origin (bool): set the origin of the X and Y axes to 0
+    :cmap (str): colormap to use, some options available,
+    :reverse (bool): reverse the colormap
+    :title (str): title of the colorbar
+    :min_max (list of float): minimum and maximum bounds of the colorbar
+    :default_range (bool): overrides min_max, if True, use the default visible
+        range for the colorbar
+    :num_colors (int): number of discrete colors in colorbar
+    :num_labels (int): number of labels on the colorbar
+    :round_to (int): only if default_range == True, rounds the bounds of the
+        default range to get cleaner min, max and step values in colorbar
+    """
+    figsize = (1000, 1000)
+    x_slices, y_slices, z_slices = None, None, None  # empty initialization
+
+    # File ID's for VTK objects that are to be plotted
+    coast_fid = "coast.npy"
+    srcs_fid = "srcs.vtk"
+    rcvs_fid = "rcvs.vtk"
+
+    # File ID's for output files
+    if not os.path.exists(path_out):
+        os.makedirs(path_out)
+
+    fid_out = os.path.join(path_out, os.path.basename(fid).split(".")[0])
+    save_fid_z = fid_out + "_z_slice_{tag}.png"
+    save_fid_x = fid_out + "_x_slice_{tag}.png"
+    save_fid_y = fid_out + "_y_slice_{tag}.png"
+
+    # Pick specific slices to create
+    z_slices = ["surface", 5, 10, 15, 30, 50]
+    # x_slices = [0.5]
+    # y_slices = [0.5]
+
+    # Set common parameters for all figures generated, passed as kwargs
+    par = {"font_factor": 1.1,
+           "convert": 1E-3,
+           "zero_origin": True,
+           "min_max": [],
+           "num_colors": 51,
+           "num_clabels": 5,
+           "round_to": None,
+           "show": False,
+           "xlabel": "E (km)",
+           "ylabel": "N (km)",
+           "zlabel": "Z (km)",
+           }
+
+
+    # Different plot types require different colors and titles    
+    if "log" in fid_out:
+        # Collect some information from the file id
+        tit = os.path.basename(fid).split(".")[0].split("_")[-1]  # e.g. vp
+        num = os.path.basename(fid).split("_")[2]  # e.g. 0003
+        if num == "init":
+            num = 0
+
+        par["title"] = f"{tit.capitalize()} log(m{int(num):0>2}/m00)"
+        par["cmap"] = "Spectral"
+        par["reverse"] = False
+        par["default_range"] = False
+    elif "poisson" in fid_out:
+        par["title"] = "Poissons Ratio"
+        par["cmap"] = "viridis" 
+        par["reverse"] = False
+        par["default_range"] = True
+    elif "gradient" in fid_out:
+        tit = os.path.basename(fid).split("_")[2]
+        num = os.path.basename(fid).split("_")[1]
+        if num == "init": 
+            num = 0
+        par["title"] = f"Grad({tit.capitalize()}) [m{int(num):0>2}]"
+        par["cmap"] = "RdBu"
+        par["reverse"] = True
+        par["default_range"] = False
+
+    # Call the model maker
+    model = ModelMaker(fid=fid, srcs=srcs_fid, rcvs=rcvs_fid, coast=coast_fid,
+                       figsize=figsize, zero_origin=par["zero_origin"],
+                       convert=par["convert"], offscreen=True, logging=False)
+
+    # Make the models
+    if z_slices:
+        par["save"] = save_fid_z
+        z_fids = []
+        for z in z_slices:
+            model.startup()
+            fid_out = model.plot_model_topdown(depth_km=z, **par)
+            z_fids.append(fid_out)
+        if make_pdf:
+            save_pdf(z_fids, save_fid_z)
+    if x_slices:
+        par["save"] = save_fid_x
+        x_fids = []
+        for x in x_slices:
+            model.startup()
+            fid_out = model.plot_depth_cross_section(choice="X",
+                                                     slice_at=x, **par)
+            x_fids.append(fid_out)
+        if make_pdf:
+            save_pdf(x_fids, save_fid_x)
+    if y_slices:
+        par["save"] = save_fid_y
+        y_fids = []
+        for y in y_slices:
+            model.startup()
+            fid_out = model.plot_depth_cross_section(choice="Y",
+                                                     slice_at=y, **par)
+            y_fids.append(fid_out)
+        if make_pdf:
+            save_pdf(y_fids, save_fid_y)
+
+
+def user_input():
+    """
+    A function to ask the user to choose from available files for making models
+
+    :rtype: list
+    :return: list of chosen filenames
+    """
+    fids = []
+    model = f"{input('Choose model number (wildcards okay): '):0>4}"
+    fids += glob(f"model_{model}_*.vtk")
+    # fids += glob(f"log_model_{model}_??.vtk")
+    # fids += glob(f"gradient_{model}_*.vtk")
+    # fids += glob(f"model_{model}_poissons.vtk")
+
+    if False:
+        add_mods = input("Make models for 'models'?: [y]/n ")
+        add_logs = input("Make models for 'log differences'?: [y]/n ")
+        add_grads = input("Make models for 'gradients'?: [y]/n ")
+        add_pois = input("Make models for 'Poissons ratios'?: [y]/n ")
+        if add_mods != "n":
+            fids += glob("model_{model}_*.vtk")
+        if add_logs != "n":
+            fids += glob(f"log_model_{model}_??.vtk")
+        if add_grads != "n":
+            fids += glob(f"gradient_{model}_*.vtk")
+        if add_pois != "n":
+            fids += glob(f"model_{model}_poissons.vtk")
+
+    print(f"{len(fids)} file(s) found")
+    return fids
+
+
+if __name__ == "__main__":
+    filenames = user_input()
+    for fid in filenames:
+        if "rcvs" in fid or "srcs" in fid:
+            continue
+        call_models(fid, make_pdf=False)
+
