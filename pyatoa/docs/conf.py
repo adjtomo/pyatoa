7f9600690fb718e6ef8bc94027617e95de9f8780
Bryant Chow <bryant.h.c@gmail.com>
2020-08-13 10:54:29 +1200

    Update conf.py

--- pyatoa/docs/conf.py
+++ pyatoa/docs/conf.py
@@ -14,7 +14,10 @@
 #
 import os
 import sys
+import pyatoa
 sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../..'))
+
 
 # -- Project information -----------------------------------------------------
 
