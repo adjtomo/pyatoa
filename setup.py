1b91df6ce28bec3758b44c39e01a6964d96d10ad
Bryant Chow <bryant.h.c@gmail.com>
2020-08-13 11:45:13 +1200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name='pyatoa',
       author='Bryant Chow',
       author_email='bryant.chow@vuw.ac.nz',
       license='GPL',
-      packages=['pyatoa'],
+      packages=['pyatoa', 'pyatoa.core', 'pyatoa.utils', 'pyatoa.visuals'],
       zip_safe=False)
