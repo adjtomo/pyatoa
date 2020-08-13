800adc4d95359b0a1f3ef20a59468339ad973a2d
Bryant Chow <bryant.h.c@gmail.com>
2020-08-13 13:08:16 +1200

    added requirements to package repo and added paths to no pypi repos in requirements

--- setup.py
+++ setup.py
@@ -8,5 +8,17 @@ setup(name='pyatoa',
       author_email='bryant.chow@vuw.ac.nz',
       license='GPL',
       packages=['pyatoa', 'pyatoa.core', 'pyatoa.utils', 'pyatoa.visuals',
-                'pyatoa.plugins'],
+                'pyatoa.plugins', 'pyatoa.utils.asdf'],
+      # install_requires=[
+      #     # 'obspy==1.2.2',
+      #     # 'pyasdf==0.7.2',
+      #     # 'pandas==1.1.0',
+      #     # 'pyyaml==5.3.1',
+      #     'repo @ http://github.com/bch0w/pyflex/tarball/return_reject_windows',
+      #     'repo @ http://github.com/krischer/pyadjoint/tarball/master'
+      #     ],
+      # dependency_link = [
+      #     'http://github.com/bch0w/pyflex/tarball/return_reject_windows',
+      #     'http://github.com/krischer/pyadjoint/tarball/master'
+      #     ],
       zip_safe=False)
