2df65f61bf338932c17ef0570e9a2113b5eefa06
bch0w <bryant.h.c@gmail.com>
2020-09-13 21:57:53 +0000

    updated pyflex presets, fixed naming in asdf clean

--- pyatoa/scripts/clean_datasets.py
+++ pyatoa/scripts/clean_datasets.py
@@ -3,9 +3,9 @@ Utility function to delete auxiliary data from all datasets in the work dir.
 """
 from glob import glob
 from pyasdf import ASDFDataSet as asdf
-from pyatoa.utils.asdf.clean import clean_ds
+from pyatoa.utils.asdf.clean import clean_dataset
 
 for fid in glob("*.h5"):
     with asdf(fid) as ds:
-        clean_ds(ds)
+        clean_dataset(ds)
         print(fid)
