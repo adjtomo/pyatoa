c210e0ee294a5392266a9e56bf8b2254a8d5ea4f
Bryant Chow <bryant.h.c@gmail.com>
2020-10-26 12:52:03 +1300

    removed accidentally committed test.py script from repo

--- test.py
+++
@@ -1,9 +0,0 @@
-import os
-path_pyatoa = os.path.dirname(os.path.realpath(__file__))
-req_file = os.path.join(path_pyatoa, "requirements.txt")
-if os.path.exists(req_file):
-    with open(req_file, "r") as f:
-        install_requires = list(f.read().splitlines())
-else:
-    install_requires = []
-print(install_requires)
