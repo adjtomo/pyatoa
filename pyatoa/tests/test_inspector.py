74dccef0147cbef9a4aa29ffaff07662a104e559
Bryant Chow <bryant.h.c@gmail.com>
2021-12-16 18:50:27 -0900

    replaced test ASDFDataSet with updated version as certain parameter keys had been updated since this dataset was placed here

---
+++ pyatoa/tests/test_inspector.py
@@ -0,0 +1,47 @@
+"""
+Test the Inspector class and its ability to generate dataframes for bulk
+analyses of an inversion
+"""
+import pytest
+from pyasdf import ASDFDataSet
+from pyatoa import Inspector, logger
+
+# Turn off logger for tests
+logger.propagate = False
+logger.setLevel("CRITICAL")
+
+
+@pytest.fixture
+def test_data():
+    return "./test_data"
+
+
+@pytest.fixture
+def asdf_dataset_fid():
+    return "./test_data/2018p130600.h5"
+
+
+@pytest.fixture
+def inspector(asdf_dataset_fid):
+    insp = Inspector()
+    insp.append(asdf_dataset_fid)
+    return insp
+
+
+def test_append(asdf_dataset_fid):
+    """
+    Inspector with data
+    """
+    insp = Inspector()
+    insp.append(asdf_dataset_fid)
+    pytest.set_trace()
+
+
+def test_discover(test_data):
+    """
+    Make sure Inspector can find HDF5 files and read them in
+    """
+    insp = Inspector()
+    insp.discover(path=test_data)
+
+
