4755344e1a9ec8a3116d952d858b0e7886d0bc7a
Bryant Chow <bryant.h.c@gmail.com>
2023-01-10 20:18:23 -0900

    added new test dataset and script to make it. updated tests to reflect new dataset

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# CHANGELOG
+
+## v0.2.1
+
+- Updated internal call structures to deal with Pyadjoint v0.2.1 API changes
+- Changed internal test ASDFDataSet and created a script to generate new dataset
+  because the old one had no way of being remade.
--- pyatoa/scripts/load_example_data.py
+++ pyatoa/scripts/load_example_data.py
@@ -16,7 +16,8 @@ def load_example_data():
     from pyatoa import Config
     from obspy import read, read_events, read_inventory
 
-    cfg = Config(event_id="2018p130600")
+    cfg = Config(event_id="2018p130600", min_period=8., max_period=20.,
+                 iteration=1, step_count=0)
     st_obs = read(os.path.join(_test_data_dir, 
                                "test_obs_data_NZ_BFZ_2018p130600.ascii"))
     st_syn = read(os.path.join(_test_data_dir, 
@@ -49,3 +50,22 @@ def load_example_asdfdataset():
                      mode="r")
 
     return ds
+
+
+def generate_example_asdfdataset():
+    """Create the test_ASDFDataSet file, which sometimes needs to be re-made if 
+    the package or dependencies change"""
+    from pyasdf import ASDFDataSet
+    from obspy import Catalog
+    from pyatoa import Manager
+
+    cfg, st_obs, st_syn, event, inv = load_example_data()
+
+    with ASDFDataSet("test_ASDFDataSet.h5") as ds:
+        cfg.write(write_to=ds)
+        ds.events = Catalog(events=[event])
+        mgmt = Manager(config=cfg, ds=ds, st_obs=st_obs, st_syn=st_syn, 
+                       event=event, inv=inv)
+        mgmt.write(ds=ds)
+        mgmt.flow()
+
