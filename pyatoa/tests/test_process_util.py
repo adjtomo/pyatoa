af25ba926a4e489a07e258e0f4685b80ea106978
Bryant Chow <bhchow@alaska.edu>
2024-05-14 17:55:12 -0800

    Bugfix Trim streams failing silently (#43)
    * added assertion checks on trim streams and created separate trim_streams test to catch this
    
    * fixed up match_npts function to deal with streams that have either less or more points, fixed test to cover this function

--- pyatoa/tests/test_process_util.py
+++ pyatoa/tests/test_process_util.py
@@ -115,25 +115,65 @@ def test_zero_pad(st_obs):
         assert(st[0].stats.npts - npts == npts_check)
 
 
-def test_trim_streams_and_match_npts(st_obs, st_syn):
+def test_trim_streams(st_obs, st_syn):
+    """
+    Test Stream trimming functionality
+    """
+    st_a = st_obs.copy()
+    st_b = st_syn.copy()
+
+    # Resample first so that we can actually trim the exact same start and 
+    # end times
+    st_a.resample(st_b[0].stats.sampling_rate)
+
+    # Trim A so we know it is different from B
+    st_a.trim(starttime=st_b[0].stats.starttime + 100,
+              endtime=st_b[0].stats.endtime - 100)
+
+    for tr_a, tr_b in zip(st_a, st_b):
+        assert(tr_a.stats.starttime != tr_b.stats.starttime)
+        assert(tr_a.stats.endtime != tr_b.stats.endtime)
+
+    # Trim streams to the same time window
+    st_at, st_bt = process.trim_streams(st_a=st_a, st_b=st_b, force="a")
+
+    for tr_a, tr_b in zip(st_at, st_bt):
+        assert(tr_a.stats.starttime == tr_b.stats.starttime)
+        assert(tr_a.stats.endtime == tr_b.stats.endtime)
+
+    # Make sure this throws an error if it is unable to trim streams
+    with pytest.raises(AssertionError):
+        process.trim_streams(st_a=st_a, st_b=st_b, force="b")
+
+
+def test_match_npts(st_obs, st_syn):
     """
     Ensure that forcing number of points standardization works
     """
     st_a = st_obs.copy()
     st_b = st_syn.copy()
 
-    # Downsample observations to synthetics
+    # Resample A to B and trim A to B
     st_a.resample(st_b[0].stats.sampling_rate)
+
+    # Trim A so we know it is different from B
+    st_a.trim(starttime=st_b[0].stats.starttime + 100,
+              endtime=st_b[0].stats.endtime - 100)
     assert(st_a[0].stats.npts != st_b[0].stats.npts)
 
-    # Trim obs data down to match syn data
-    st_a, st_b = process.trim_streams(st_a=st_a, st_b=st_b, force="b")
-    assert(st_a[0].stats.npts == st_b[0].stats.npts)
+    # Trim streams so that they are the same length
+    st_a, st_b = process.trim_streams(st_a=st_a, st_b=st_b, force="a")
 
-    # Purposefully mismatch npts and then pad with 0s
-    st_a[0].trim(endtime=st_a[0].stats.endtime - 1)
-    st_a, st_b = process.match_npts(st_a, st_b, force="b")
-    assert(st_a[0].stats.npts == st_b[0].stats.npts)
+    # Force one stream to be slightly shorter
+    st_a.trim(endtime=st_a[0].stats.endtime - 1)
+    
+    # Check they're different
+    for tr_a, tr_b in zip(st_a, st_b):
+        assert(tr_a.stats.npts != tr_b.stats.npts)
+
+    # Match npts
+    st_at, st_bt = process.match_npts(st_a, st_b, force="b")
+    assert(st_at[0].stats.npts == st_bt[0].stats.npts)
 
 
 def test_is_preprocessed(st_obs):
